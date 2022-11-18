65735557d6d8a61f4d31842ea2b1ef904d51d7ab
liyuKnow <kidus2011@gmail.com>
2022-11-17 19:20:59 -0800

    basic setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+
+  return (
+    <div className="container">
+      <h1>HELLO HELLO !!</h1>
+    </div>
+  )
+}
+
+export default App
