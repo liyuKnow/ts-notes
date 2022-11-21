1a42550c45d047b0efd8bf1e0959866178e6329e
liyuKnow <kidus2011@gmail.com>
2022-11-20 22:10:19 -0800

    last commit

--- src/components/pages/public/Home.tsx
+++ src/components/pages/public/Home.tsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import { useMemo } from 'react'
+import { useLocalStorage } from '../../../hooks/useLocalStorage';
+import { NoteData, RawNote, Tag } from '../../../types';
+import { v4 as uuidV4 } from "uuid";
 
 const Home = () => {
-    return (
-        <div>Home</div>
-    )
+  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", []);
+  const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", []);
+
+  const notesWithTags = useMemo(() => {
+    return notes.map(note => {
+      return {
+        ...note,
+        tags: tags.filter(
+          tag => note.tagIds.includes(tag.id))
+      }
+    })
+  }, [notes, tags]);
+
+  const onCreateNote = (data: NoteData) => {
+    setNotes(prevNotes => {
+      return [...prevNotes, {
+        ...data,
+        id: uuidV4(),
+        tagIds: tags.map(tag => tag.id)
+      }]
+    })
+  }
+
+  return (
+    <div>Home</div>
+  )
 }
 
 export default Home
